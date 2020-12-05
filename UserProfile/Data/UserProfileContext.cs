653ebe5248dd9b2c43bf2cfe92d8f7991954fd79
Navneet Kang <navneettech@yahoo.com>
2020-12-04 20:39:38 -0500

    Authentication

---
+++ UserProfile/Data/UserProfileContext.cs
@@ -0,0 +1,49 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserProfile.Models;
+
+namespace UserProfile.Data
+{
+    public class UserProfileContext  : DbContext
+    {
+
+        public UserProfileContext(DbContextOptions<UserProfileContext> options)
+: base(options)
+        {
+
+        }
+
+
+        /*
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+         {
+             base.OnModelCreating(modelBuilder);
+             modelBuilder.Entity<BlogModel>().HasOne<UserDetails>(p=>p.use).WithMany(p=>p.Blogs);
+             modelBuilder.Entity<UserDetails>().HasMany<BlogModel>(p=>p.Blogs).WithOne(p=>p.);
+
+         }
+
+        */
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+           
+           
+        }
+
+        public DbSet<UserDetails> UserDetails { get; set; }
+        public DbSet<SocialDetails> SocialDetails{ get; set; }
+        public DbSet<BlogModel> BlogDetails { get; set; }
+        
+
+
+
+    }
+}
